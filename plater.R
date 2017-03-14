ed3b756f4ab6425c8315639286d9421aac5ebff9
Peter Higgins <higgi13425@yahoo.com>
2017-03-13 22:11:06 -0400

    updated with mean Ct, facet grid

--- plater.R
+++ plater.R
@@ -14,9 +14,9 @@ library(readr)
 rm(list=ls())
 
 #plan experiment
-# three variables: drug, concentration in nM, TGFb Rx
+# five variables: gene, drug, concentration in nM, TGFb Rx, template present
 #create plater.csv file of plate plan
-#read in plater.csv
+#read in plater.csv that reflects plate experiment design
 plate<- read_plate("plater.csv")
 
 
@@ -29,6 +29,18 @@ df<-filter(df, !is.na(label)) #remove empty rows
 df$Ct<- as.numeric(df$Ct) #make Ct numeric
 df$num<-str_replace(df$num,"TGF-9-2-16_","") # clean up numbers
 
+#change well labels from A1 to A01 so all are 3 characters long, facilitating join
 df$Wells[as.numeric(str_sub(df$Wells,2,3))<10]<-str_replace(df$Wells,str_sub(df$Wells,2,2), paste0("0",str_sub(df$Wells,2,2)))[as.numeric(str_sub(df$Wells,2,3))<10]
 
+#now do the full join
 full<-full_join(df,plate) %>% select(-label,-num)
+
+#now summarize replicates
+full %>%
+  group_by(gene, TGFbeta, drug, conc, template) %>% 
+  summarize (mCt=mean(Ct))
+
+# try a facet graph
+full %>% 
+  filter(template==1, gene=="col1A1") %>% 
+ggplot(aes(x=conc, y=Ct)) + geom_jitter() + facet_grid(drug ~ TGFbeta)
